c8cae544c137aa5ec1695a18d54a7c94738f66ac
Hadrien Barbat <>
2021-05-26 14:11:18 +0200

    fetch from backend

--- Controller/CrowdFetcher.swift
+++ Controller/CrowdFetcher.swift
@@ -56,7 +56,8 @@ class CrowdFetcher: ObservableObject {
     
     init() {
         frekPlaces = ValueStore().frekPlaces
-        fetchAll()
+        // fetchAll()
+        fetchBackend()
     }
     
     func isDataSourceReady() -> Bool {
@@ -64,7 +65,8 @@ class CrowdFetcher: ObservableObject {
     }
     
     func refresh() {
-        fetchAll()
+        // fetchAll()
+        fetchBackend()
     }
     
     func updateData() {
@@ -72,6 +74,42 @@ class CrowdFetcher: ObservableObject {
         ComplicationController.reloadAllComplicationsData()
         #endif
     }
+    
+    func fetchBackend() {
+        refreshing = true
+        HTTPHelper.httpRequest(endpoint: "https://fedutia.fr:8003", method: .get, params: nil) { result in
+            switch result {
+            case .success(let dictFrekPlaces):
+                self.onFrekPlacesFetched(dictFrekPlaces)
+                print("✅ \(self.frekPlaces.count) FrekPlacecs created!")
+            case .failure(let err):
+                print("❌ Error fetching backend: \(err)")
+            }
+        }
+    }
+    
+    func onFrekPlacesFetched(_ frekPlaces: [Dict]) {
+        frekPlaces.forEach { frekPlaceDict in
+            if let frekPlace = FrekPlace.decode(frekPlaceDict) {
+                DispatchQueue.main.async {
+                    if let index = self.frekPlaces.firstIndex(where: { $0.id == frekPlace.id }) {
+                        self.updateFrekPlace(at: index, frekPlace)
+                    } else {
+                        self.frekPlaces.append(frekPlace)
+                    }
+                }
+            }
+        }
+    }
+    
+    func updateFrekPlace(at index: Int, _ frekPlace: FrekPlace) {
+        frekPlaces[index].crowd = frekPlace.crowd
+        frekPlaces[index].spotsAvailable = frekPlace.spotsAvailable
+        frekPlaces[index].fmi = frekPlace.fmi
+        frekPlaces[index].state = frekPlace.state
+        // frekPlaces[index].image = frekPlace.image
+    }
+    
     
     func fetchAll() {
         refreshing = true
