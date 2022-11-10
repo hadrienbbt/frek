f749c696fe7b36f4e46f3d2adb2475e48d123922
Hadrien Barbat <hadrien@fedutia.fr>
2022-11-10 22:35:37 +0100

    removed app delegate

--- Source/View/IOSView/MainView.swift
+++ Source/View/IOSView/MainView.swift
@@ -2,8 +2,6 @@ import SwiftUI
 
 @main
 struct MainView: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-
     var body: some Scene {
         return WindowGroup {
             FrekPlaceList()
